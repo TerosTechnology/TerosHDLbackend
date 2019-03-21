b9da6e85f9a963ef041e7e51358b0bba38fc6c25
ismael <ismael@ismael-L755>
2019-03-21 23:58:08 +0100

    Update headers

--- terosHdlBackend/terosHdlRepo/hdlRepoClass.py
+++ terosHdlBackend/terosHdlRepo/hdlRepoClass.py
@@ -1,19 +1,19 @@
-# Copyright 2018 DAS Photonics
+# Copyright 2019 Carlos Alberto Ruiz Naranjo, Ismael Pérez Rojo
 #
-# This file is part of ATOMato.
+# This file is part of TerosHDL.
 #
-# ATOMato is free software: you can redistribute it and/or modify
+# TerosHDL is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Atomato is distributed in the hope that it will be useful,
+# TerosHDL is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Atomato.  If not, see <https://www.gnu.org/licenses/>.
+# along with TerosHDL.  If not, see <https://www.gnu.org/licenses/>.
 
 
 # -*- coding: utf-8 -*-
